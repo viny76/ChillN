82b892ba21ec0011ef300a494b1ef64bbb0ccf01
Vincent Jardel <Viny@MacBook-Pro-de-Vincent.local>
2015-03-26 21:54:37 +0000

    Initial Commit

---
+++ ChillN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChillN
+//
+//  Created by Vincent Jardel on 26/03/2015.
+//  Copyright (c) 2015 ChillCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChillN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChillN
+//
+//  Created by Vincent Jardel on 26/03/2015.
+//  Copyright (c) 2015 ChillCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
